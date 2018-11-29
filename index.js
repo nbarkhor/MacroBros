44ec6e4dd592b74867fe0096622502840279500f
Priya <priyarkan@gmail.com>
2018-11-29 10:29:14 -0800

    fixed sign in/sign up glitch

--- index.js
+++ index.js
@@ -12,19 +12,29 @@ function writeUserData() {
 	var u = document.getElementById("emailq").value
 	var p =  document.getElementById("passq").value
 	var n = document.getElementById("nameq").value
-	firebase.auth().createUserWithEmailAndPassword(u, p).catch(function(err) {
+		window.open("macropercent.html", "_self");
+	}).catch(function(err) {
   	 // Handle errors
   	 alert("could not make new user");
  	});
-	window.open("https://nbarkhor.github.io/cs188Project/macropercent.html", "_self");
 }
+
 
 function signIn() {
 	var u = document.getElementById("emailq").value
 	var p = document.getElementById("passq").value
-	firebase.auth().signInWithEmailAndPassword(u, p).catch(function(error) {
+	firebase.auth().signInWithEmailAndPassword(u, p).then(function() {
+		window.open("macropercent.html", "_self");
+	})catch(function(error) {
   	// Handle Errors here.
   		alert("could not sign in");
 	});
-	window.open("https://nbarkhor.github.io/cs188Project/macropercent.html", "_self");
+}
+
+function signOut() {
+	firebase.auth().signOut().then(function() {
+  // Sign-out successful.
+	}).catch(function(error) {
+  // An error happened.
+});
 }
