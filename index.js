efbc159874b5178c87f5a3bd542c06c79349d5fd
shreyk98 <shreyk98@g.ucla.edu>
2018-11-29 15:32:07 -0800

    functions loading from js file now
    had to comment catch tho

--- index.js
+++ index.js
@@ -8,26 +8,48 @@
 
 var database = firebase.database().ref();
 
-var recipedb = firebase.database().ref().child('Chicken').child('bread');
+var recipedb = firebase.database().ref().child('Recipes').child('Chicken').child('Bread');
 
 function addnewTable(){
 	console.log("T1");
-	recipedb.once('value', function(snapshot){
-	    if(snapshot.exists()){
-	        var content = '';
-
-	        snapshot.forEach(function(data){
-	            var name = data.val().Name;
-	            var time= data.val().Time;
+		recipedb.once('value', function(snapshot){
+			var content = '';
+			snapshot.forEach(function(childSnapshot) {
+			    var childKey = childSnapshot.key;
+			    // var childData = childSnapshot.val();
+			    // console.log(childSnapshot.val().Name)
+	            var name = childSnapshot.val().Name;
+	            var time= childSnapshot.val().Time;
 	            content += '<tr>';
 	            content += '<td>' + name + '</td>'; //column1
 	            content += '<td>' + time + '</td>';//column2
 	            content += '</tr>';
-	        });
 
-	        $('#trial').append(content);
-	    }
-	});
+			});
+			console.log(content)
+			$('#trial').prepend(content);
+
+		    // if(snapshot.exists()){
+		    //     var content = '';
+		    //     console.log("T2")
+		    //     snapshot.forEach(function(data){
+		    //         var name = data.val().Name;
+		    //         var time= data.val().Time;
+		    //         content += '<tr>';
+		    //         content += '<td>' + name + '</td>'; //column1
+		    //         content += '<td>' + time + '</td>';//column2
+		    //         content += '</tr>';
+		    //     });
+
+
+		    //     $('#trial').append(content);
+		    // }
+		    // else{
+		    // 	console.log("T21")
+
+		    // }
+		});
+		console.log("t3")
 
 }
 
@@ -56,11 +78,12 @@ function signIn() {
 	var p = document.getElementById("passq").value
 	firebase.auth().signInWithEmailAndPassword(u, p).then(function() {
 		window.open("macropercent.html", "_self");
-	})catch(function(error) {
-  	// Handle Errors here.
-  		console.log("This error happened?")
-  		alert("could not sign in");
-	});
+	})
+	// catch(function(error) {
+ //  	// Handle Errors here.
+ //  		console.log("This error happened?")
+ //  		alert("could not sign in");
+	// });
 }
 
 function signOut() {
