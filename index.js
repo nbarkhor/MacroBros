f8e64650f10434aa58faac346b540fe305f5ce65
Priya <priyarkan@gmail.com>
2018-11-28 23:04:57 -0800

    signin with email password

--- index.js
+++ index.js
@@ -10,12 +10,21 @@ var database = firebase.database().ref();
 
 function writeUserData() {
 	var u = document.getElementById("emailq").value
-	console.log(u)
 	var p =  document.getElementById("passq").value
 	var n = document.getElementById("nameq").value
 	firebase.auth().createUserWithEmailAndPassword(u, p).catch(function(err) {
   	 // Handle errors
   	 alert("could not make new user");
  	});
+	window.open("https://nbarkhor.github.io/cs188Project/macropercent.html", "_self");
+}
+
+function signIn() {
+	var u = document.getElementById("emailq").value
+	var p = document.getElementById("passq").value
+	firebase.auth().signInWithEmailAndPassword(u, p).catch(function(error) {
+  	// Handle Errors here.
+  		alert("could not sign in");
+	});
 	window.open("https://nbarkhor.github.io/cs188Project/macropercent.html", "_self");
 }
